2cb5b9ae7dfaf6f7bc4827feeab298c818199779
Ingo <roessner@infai.org>
2021-04-21 13:32:12 +0200

    Revert "add device and service info to process deployment message"
    This reverts commit de167eab71b2c563002c320e347c8d2d395ad011.

--- pkg/controller/sourcingreplacement.go
+++ pkg/controller/sourcingreplacement.go
@@ -22,11 +22,7 @@ import (
 	"errors"
 	"github.com/SENERGY-Platform/process-deployment/lib/config"
 	"github.com/SENERGY-Platform/process-deployment/lib/interfaces"
-	"github.com/SENERGY-Platform/process-deployment/lib/model/deploymentmodel/v2"
-	"github.com/SENERGY-Platform/process-deployment/lib/model/devicemodel"
 	"github.com/SENERGY-Platform/process-deployment/lib/model/messages"
-	"github.com/SENERGY-Platform/process-fog-deployment/pkg/model"
-	jwt_http_router "github.com/SmartEnergyPlatform/jwt-http-router"
 )
 
 //mocks sourcing interface to reuse github.com/SENERGY-Platform/process-deployment/lib/ctrl without connecting to kafka
@@ -34,7 +30,6 @@ type SourcingReplacement struct {
 	token       string
 	hubId       string
 	processSync ProcessSync
-	devicerepo  interfaces.Devices
 }
 
 func (this *SourcingReplacement) NewConsumer(ctx context.Context, config config.Config, topic string, listener func(delivery []byte) error) error {
@@ -46,7 +41,6 @@ type ProducerReplacement struct {
 	token       string
 	hubId       string
 	processSync ProcessSync
-	devicerepo  interfaces.Devices
 }
 
 func (this *ProducerReplacement) Produce(topic string, message []byte) error {
@@ -58,43 +52,7 @@ func (this *ProducerReplacement) Produce(topic string, message []byte) error {
 	if deplMsg.DeploymentV2 == nil {
 		return errors.New("expect deployment v2 in ProducerReplacement.Produce()")
 	}
-	fogDeplMessage, err := this.createFogDeploymentMessage(this.token, *deplMsg.DeploymentV2)
-	if err != nil {
-		return err
-	}
-	return this.processSync.Deploy(this.token, this.hubId, fogDeplMessage)
-}
-
-func (this *ProducerReplacement) createFogDeploymentMessage(token string, deployment deploymentmodel.Deployment) (result model.FogDeploymentMessage, err error) {
-	result.Deployment = deployment
-	result.Devices = map[string]devicemodel.Device{}
-	result.Services = map[string]devicemodel.Service{}
-	for _, element := range deployment.Elements {
-		var selection deploymentmodel.Selection
-		if element.MessageEvent != nil {
-			selection = element.MessageEvent.Selection
-		}
-		if element.Task != nil {
-			selection = element.Task.Selection
-		}
-		if selection.SelectedDeviceId == nil {
-			return result, errors.New("expect selected device id")
-		}
-		if selection.SelectedServiceId == nil {
-			return result, errors.New("expect selected service id")
-		}
-		device, err, _ := this.devicerepo.GetDevice(jwt_http_router.JwtImpersonate(token), *selection.SelectedDeviceId)
-		if err != nil {
-			return result, err
-		}
-		service, err, _ := this.devicerepo.GetService(jwt_http_router.JwtImpersonate(token), *selection.SelectedServiceId)
-		if err != nil {
-			return result, err
-		}
-		result.Devices[device.Id] = device
-		result.Services[service.Id] = service
-	}
-	return result, nil
+	return this.processSync.Deploy(this.token, this.hubId, *deplMsg.DeploymentV2)
 }
 
 func (this *SourcingReplacement) NewProducer(ctx context.Context, config config.Config, topic string) (interfaces.Producer, error) {
@@ -102,6 +60,5 @@ func (this *SourcingReplacement) NewProducer(ctx context.Context, config config.
 		token:       this.token,
 		hubId:       this.hubId,
 		processSync: this.processSync,
-		devicerepo:  this.devicerepo,
 	}, nil
 }
